5eb1f08ab63cc21a8c2f4b572ab80daae3b3922f
slimslenderslacks <slimslenderslacks@gmail.com>
2018-03-19 12:39:32 -0700

    Version 0.6.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject minikube-test "0.1.0"
+(defproject minikube-test "0.6.2"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "1.1.11"]
